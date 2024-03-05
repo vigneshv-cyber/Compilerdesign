b6a2a7c97c15fce5bed440bd3900221961566dd5
vignesh_cyber <113492992+vigneshv-cyber@users.noreply.github.com>
2024-03-05 08:10:23 +0530

    Add files via upload

---
+++ validatigoperators.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    printf("Enter an arithmetic operator (+, -, *, /): "); /*Vignesh*/
+    scanf(" %c", &op);//compiler design//
+    if (op == '+') {
+        printf("\n\t+ --> Addition (Valid Input)", op);
+    } else if(op == '-') {
+        printf("\n\t- --> Subtraction (Valid Input)", op);
+    }else if(op == '*') {
+        printf("\n\t* --> Multiplication (Valid Input)", op);
+    }else if(op == '/') {
+        printf("\n\t/ --> Division (Valid Input)", op);
+    }
+    else{
+    	printf("\n\tInvalid Input");
+	}
+    return 0;
+}
+
